c88bb44e9f07202ab89d2a9273b75fb24486940a
AbdulKalamU <uabdulkalam@gmail.com>
2026-02-15 19:04:59 +0000

    Update to V2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('DevSecOps Pipeline version 1.0! The pipeline is working!');
+  res.end('Version 2.0! Watchtower CD was successful!');
 });
 
 server.listen(8080, () => {
